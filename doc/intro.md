806251175a15f8ee5c958937a8a9d60318a62258
john <decker49@gmail.com>
2015-04-24 18:37:07 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to buildkittens
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/buildkittens/core_test.clj
@@ -0,0 +1,7 @@
+(ns buildkittens.core-test
+  (:require [clojure.test :refer :all]
+            [buildkittens.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
